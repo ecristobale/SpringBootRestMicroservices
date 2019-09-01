43e6b10171685fe1c36d87bc8798095b38d5949a
ecristobale <edu_ce_1988@hotmail.com>
2019-09-01 17:13:58 +0200

    Initial commit, creation of 3 simple microservices. Developing of the 1st microservice: MovieCatalogService Execute in: localhost:8080/catalog/theUserIdYouLike

---
+++ movie-catalog-service/src/main/java/com/ecristobale/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,21 @@
+package com.ecristobale.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecristobale.moviecatalogservice.model.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+		return Collections.singletonList(
+				new CatalogItem("Film " + userId, "This is the film description for the film with userId = " + userId, 9));
+	}
+}
