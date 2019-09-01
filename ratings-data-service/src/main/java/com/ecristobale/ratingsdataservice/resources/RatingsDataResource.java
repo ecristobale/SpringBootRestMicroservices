a613d81a3df2090f14f5952ce3d429f54a1494ba
ecristobale <edu_ce_1988@hotmail.com>
2019-09-01 18:03:26 +0200

    Implementing and configuring RatingsData microservice in order to run on 8083 port.

---
+++ ratings-data-service/src/main/java/com/ecristobale/ratingsdataservice/resources/RatingsDataResource.java
@@ -0,0 +1,17 @@
+package com.ecristobale.ratingsdataservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ecristobale.ratingsdataservice.models.Rating;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsDataResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId") String movieId) {
+		return new Rating(movieId, 8);
+	}
+}
